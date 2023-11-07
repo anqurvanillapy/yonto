2ef1289ef3e97b6c74ce978ddf54df68c770149d
anqurvanillapy <anqurvanillapy@gmail.com>
2023-11-07 09:27:52 +0800

    Refactoring
    Signed-off-by: anqurvanillapy <anqurvanillapy@gmail.com>

--- joben.c
+++ joben.c
@@ -403,9 +403,6 @@ skip_spaces(struct source *s)
 
 struct fn;
 struct parser;
-struct source *parse(struct parser *p, struct source *s);
-
-enum parser_kind { WORD, LOWERCASE, ALL, ANY, MANY, FN, FN_PARAMS, FN_PARAM };
 
 union parser_data {
     const char *word;
@@ -418,18 +415,12 @@ union parser_data {
 };
 
 struct parser {
-    enum parser_kind kind;
+    struct source *(*parse)(union parser_data *data, struct source *s);
     union parser_data data;
 };
 
-static struct parser _LPAREN = {WORD, {.word = "("}};
-static struct parser _RPAREN = {WORD, {.word = ")"}};
-static struct parser _COMMA = {WORD, {.word = ","}};
-static struct parser _FN = {WORD, {.word = "fn"}};
-static struct parser _ASSIGN = {WORD, {.word = "="}};
-
-static struct source *
-_parse_word(union parser_data *p, struct source *s)
+struct source *
+word(union parser_data *p, struct source *s)
 {
     const char *word = p->word;
     size_t i = 0;
@@ -442,8 +433,14 @@ _parse_word(union parser_data *p, struct source *s)
     return s;
 }
 
-static struct source *
-_parse_lowercase(union parser_data *p, struct source *s)
+static struct parser _LPAREN = {word, {.word = "("}};
+static struct parser _RPAREN = {word, {.word = ")"}};
+static struct parser _COMMA = {word, {.word = ","}};
+static struct parser _FN = {word, {.word = "fn"}};
+static struct parser _ASSIGN = {word, {.word = "="}};
+
+struct source *
+lowercase(union parser_data *p, struct source *s)
 {
     struct loc start = s->loc;
 
@@ -467,11 +464,11 @@ _parse_lowercase(union parser_data *p, struct source *s)
 }
 
 struct source *
-_parse_all(union parser_data *p, struct source *s)
+all(union parser_data *p, struct source *s)
 {
     struct parser **parser = p->parsers;
     while (*parser) {
-        s = parse((*parser), s);
+        s = (*parser)->parse(&(*parser)->data, s);
         if (s->failed) {
             return s;
         }
@@ -483,12 +480,12 @@ _parse_all(union parser_data *p, struct source *s)
     return s;
 }
 
-static struct source *
-_parse_any(union parser_data *p, struct source *s)
+struct source *
+any(union parser_data *p, struct source *s)
 {
     struct loc loc = s->loc;
     for (struct parser **parser = p->parsers; *parser; parser++) {
-        s = parse(*parser, s);
+        s = (*parser)->parse(&(*parser)->data, s);
         if (!s->failed) {
             return s;
         }
@@ -498,12 +495,12 @@ _parse_any(union parser_data *p, struct source *s)
     return s;
 }
 
-static struct source *
-_parse_many(union parser_data *p, struct source *s)
+struct source *
+many(union parser_data *p, struct source *s)
 {
     while (1) {
         struct loc loc = s->loc;
-        s = parse(p->parser, s);
+        s = p->parser->parse(&p->parser->data, s);
         if (s->failed) {
             return source_back(s, loc);
         }
@@ -516,12 +513,12 @@ struct param {
     struct span name;
 };
 
-static struct source *
-_parse_fn_param(union parser_data *p, struct source *s)
+struct source *
+fn_param(union parser_data *p, struct source *s)
 {
     struct span name;
-    struct parser name_parser = {LOWERCASE, {.span = &name}};
-    s = parse(&name_parser, s);
+    union parser_data name_parser = {.span = &name};
+    s = lowercase(&name_parser, s);
     if (s->failed) {
         return s;
     }
@@ -533,22 +530,22 @@ _parse_fn_param(union parser_data *p, struct source *s)
     return s;
 }
 
-static struct source *
-_parse_fn_params(union parser_data *p, struct source *s)
+struct source *
+fn_params(union parser_data *p, struct source *s)
 {
     struct parser *no_params[] = {&_LPAREN, &_RPAREN, NULL};
-    struct parser all_no_params = {ALL, {.parsers = no_params}};
+    struct parser all_no_params = {all, {.parsers = no_params}};
 
-    struct parser one_param = {FN_PARAM, {.nodes = p->nodes}};
+    struct parser one_param = {fn_param, {.nodes = p->nodes}};
     struct parser *other_params[] = {&_COMMA, &one_param, NULL};
-    struct parser all_other_params = {ALL, {.parsers = other_params}};
-    struct parser many_other_params = {MANY, {.parser = &all_other_params}};
+    struct parser all_other_params = {all, {.parsers = other_params}};
+    struct parser many_other_params = {many, {.parser = &all_other_params}};
     struct parser *multi_params[] = {&_LPAREN, &one_param, &many_other_params, &_RPAREN, NULL};
-    struct parser all_multi_params = {ALL, {.parsers = multi_params}};
+    struct parser all_multi_params = {all, {.parsers = multi_params}};
 
     struct parser *branches[] = {&all_no_params, &all_multi_params, NULL};
-    struct parser any_branches = {ANY, {.parsers = branches}};
-    return parse(&any_branches, s);
+    union parser_data any_branches = {.parsers = branches};
+    return any(&any_branches, s);
 }
 
 struct fn {
@@ -556,24 +553,14 @@ struct fn {
     struct node *params;
 };
 
-static struct source *
-_parse_fn(union parser_data *p, struct source *s)
-{
-    struct parser name = {LOWERCASE, {.span = &p->fn->name}};
-    struct parser params = {FN_PARAMS, {.nodes = &p->fn->params}};
-    struct parser *parsers[] = {&_FN, &name, &params, &_ASSIGN, NULL};
-    struct parser all_fn = {ALL, {.parsers = parsers}};
-    return parse(&all_fn, s);
-}
-
-static struct source *(*_PARSE[])(union parser_data *data, struct source *s) = {
-    _parse_word, _parse_lowercase, _parse_all, _parse_any, _parse_many, _parse_fn, _parse_fn_params, _parse_fn_param,
-};
-
 struct source *
-parse(struct parser *p, struct source *s)
+fn(union parser_data *p, struct source *s)
 {
-    return _PARSE[p->kind](&p->data, s);
+    struct parser name = {lowercase, {.span = &p->fn->name}};
+    struct parser params = {fn_params, {.nodes = &p->fn->params}};
+    struct parser *parsers[] = {&_FN, &name, &params, &_ASSIGN, NULL};
+    union parser_data all_fn = {.parsers = parsers};
+    return all(&all_fn, s);
 }
 
 struct app {
@@ -628,8 +615,8 @@ main(int argc, const char *argv[])
     }
 
     struct fn f;
-    struct parser fn_parser = {FN, {.fn = &f}};
-    struct source *s = parse(&fn_parser, &app.src);
+    union parser_data fn_parser = {.fn = &f};
+    struct source *s = fn(&fn_parser, &app.src);
     if (s->failed) {
         printf("parse function error: pos=%lu\n", s->loc.pos);
         return 1;
